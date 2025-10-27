018e5165cbbe49e8c3f7d5a0b253942171d09727
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-10-27 17:25:34 +0530

    signals objects updation

--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, computed } from '@angular/core';
+import { Component, OnInit, signal, computed, effect } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -13,6 +13,12 @@ export class SignalComponent implements OnInit {
   // Basic Writable signals
   firstName = signal<string>('Shubham');
   lastName = signal<string>('Suryawanshi');
+  userObj = signal<any>({
+    firstName: 'Shubhankar',
+    lastName: 'Mishra',
+    age: 30,
+    city: 'Nagpur'
+  })
 
   // basic computed signals
   a = signal<number>(0);
@@ -26,6 +32,20 @@ export class SignalComponent implements OnInit {
 
   // make fullName computed as well
   fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+
+  constructor() {
+    effect(() => {
+      let firstName = this.firstName();
+      
+      this.userObj.update(user => ({
+        ...user,
+        firstName: firstName
+      }));
+      
+      console.log(firstName, this.userObj());
+      console.log('first name has been changed:', this.userObj());
+    }, { allowSignalWrites: true });
+  }
 
   ngOnInit(): void {
     setTimeout(() => {
@@ -42,7 +62,10 @@ export class SignalComponent implements OnInit {
       console.log('Addition of a & b  = c (computed):', this.c());
     }, 2000);
 
+  }
 
+  changeFName() {
+    this.firstName.set('Santosh');
   }
 
 
