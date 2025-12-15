99db4732f8f1ef2f88a21399940527135f899dd5
Shubham Suryawanshi <shubhsurya06@gmail.com>
2025-12-15 19:20:57 +0530

    Structural directive with TEMPLATE CONTEXT

--- src/app/reusable-component/hide-after.directive.ts
+++ src/app/reusable-component/hide-after.directive.ts
@@ -1,13 +1,13 @@
 import { Directive, ViewContainerRef, TemplateRef, OnInit, Input } from '@angular/core';
 
-// class HideAfterContext {
-//   // public $implicit = 1000;
-//   public get $implicit() {
-//     return this.hideAfter;
-//   }
-//   public hideAfter = 0;
-//   public counter = 0;
-// }
+class HideAfterContext {
+  // public $implicit = 1000;
+  public get $implicit() {
+    return this.hideAfter;
+  }
+  public hideAfter = 0;
+  public counter = 0;
+}
 
 @Directive({
   selector: '[hideAfter]',
@@ -15,59 +15,59 @@ import { Directive, ViewContainerRef, TemplateRef, OnInit, Input } from '@angula
 })
 export class HideAfterDirective implements OnInit {
 
-  @Input("hideAfter")
-  delay = 0;
+  // @Input("hideAfter")
+  // delay = 0;
 
-  @Input("hideAfterThen")
-  placeholder: TemplateRef<any> | null = null;
-
-  constructor(
-    private viewContainerRef: ViewContainerRef,
-    private template: TemplateRef<any>
-  ) {}
-
-  ngOnInit(): void {
-    this.viewContainerRef.createEmbeddedView(this.template);
-    setTimeout(() => {
-      this.viewContainerRef.clear();
-      if (this.placeholder) {
-        this.viewContainerRef.createEmbeddedView(this.placeholder);
-      }
-    }, this.delay);
-  }
-
-  // @Input()
-  // hideAfter = 0;
-
-  // @Input('hideAfter')
-  // set delay(value: number | null) {
-  //   this._delay = value ?? 0;
-  //   // this.context.counter = this._delay / 1000;
-  //   this.context.hideAfter = this.context.counter = this._delay / 1000;
-  // }
-  // private _delay: number = 0;
-
-  // private context = new HideAfterContext();
-
-  // @Input('hideAfterThen')
+  // @Input("hideAfterThen")
   // placeholder: TemplateRef<any> | null = null;
 
-  // constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>) { }
+  // constructor(
+  //   private viewContainerRef: ViewContainerRef,
+  //   private template: TemplateRef<any>
+  // ) {}
 
   // ngOnInit(): void {
-  //   this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
-
-  //   const intervalId = setInterval(() => {
-  //     this.context.counter--;
-  //   }, 1000);
-
+  //   this.viewContainerRef.createEmbeddedView(this.template);
   //   setTimeout(() => {
   //     this.viewContainerRef.clear();
   //     if (this.placeholder) {
-  //       this.viewContainerRef.createEmbeddedView(this.placeholder, this.context);
+  //       this.viewContainerRef.createEmbeddedView(this.placeholder);
   //     }
-  //     clearInterval(intervalId);
-  //   }, this._delay);
+  //   }, this.delay);
   // }
+
+  @Input()
+  hideAfter = 0;
+
+  @Input('hideAfter')
+  set delay(value: number | null) {
+    this._delay = value ?? 0;
+    // this.context.counter = this._delay / 1000;
+    this.context.hideAfter = this.context.counter = this._delay / 1000;
+  }
+  private _delay: number = 0;
+
+  private context = new HideAfterContext();
+
+  @Input('hideAfterThen')
+  placeholder: TemplateRef<any> | null = null;
+
+  constructor(private viewContainerRef: ViewContainerRef, private templateRef: TemplateRef<any>) { }
+
+  ngOnInit(): void {
+    this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
+
+    const intervalId = setInterval(() => {
+      this.context.counter--;
+    }, 1000);
+
+    setTimeout(() => {
+      this.viewContainerRef.clear();
+      if (this.placeholder) {
+        this.viewContainerRef.createEmbeddedView(this.placeholder, this.context);
+      }
+      clearInterval(intervalId);
+    }, this._delay);
+  }
 
 }
